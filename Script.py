9e23b8290e0aaaff926793b3e6150613b1bc2a58
srilinks4k <116558839+belikeberlinQ@users.noreply.github.com>
2022-11-27 13:40:03 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,6 +4,7 @@ class script(object):
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """🤖 Bot Name: {}
+    
 👨‍💻 Developer: <a href=https://t.me/Belikeberlin>Berlin</a>
 
 📚 Framework: Pyrogram
